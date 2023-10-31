9cc67b69a263b332771c49c4c3f88bd592cee5cc
小雨 <84885148+tangxiangong@users.noreply.github.com>
2023-10-31 10:38:26 +0800

    add `matrix` and `vector`

--- main.c
+++ main.c
@@ -2,19 +2,22 @@
 //#include <julia.h>
 //JULIA_DEFINE_FAST_TLS
 #include "test.h"
+#include "vector.h"
+#include "matrix.h"
 
 double func(double x)
 {
     return x*x;
 }
 
-
 int main() {
-    double a = 0;
-    double b = 1;
-    double dx=1e-3;
-    double result = trapezoid(func, a, b, dx);
-    printf("result = %f\n", result);
-    printf("Hello, World!\n");
+    double a[] = {1, 2, 3, 4};
+    double b[] = {-1,-2,-4,-3};
+    Vector v1 = {a, 4};
+    Vector v2 = {b, 4};
+    Vector* v = vec_add(&v1, &v2);
+    for(int i=0;i<4;i++)
+        printf("%f ", v->data[i]);
+    printf("\nHello, World!\n");
     return 0;
 }
